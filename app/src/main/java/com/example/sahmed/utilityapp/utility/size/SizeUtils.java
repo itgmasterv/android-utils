27061d4d7d14bd1d9f9d8494ac4037e8cfa3d38f
syasein <syasien@egypt.itgsolutions.com>
2017-10-18 16:03:57 +0200

    size

---
+++ app/src/main/java/com/example/sahmed/utilityapp/utility/size/SizeUtils.java
@@ -0,0 +1,183 @@
+package com.example.sahmed.utilityapp.utility.size;
+
+import android.content.Context;
+import android.util.DisplayMetrics;
+import android.util.TypedValue;
+import android.view.View;
+import android.view.ViewGroup;
+
+/**
+ * <pre>
+ *     author: Blankj
+ *     blog  : http://blankj.com
+ *     time  : 2016/08/02
+ *     desc  : Dimensions related tools
+ * </pre>
+ */
+public final class SizeUtils {
+
+    private SizeUtils() {
+        throw new UnsupportedOperationException("u can't instantiate me...");
+    }
+
+    /**
+     * dp to px
+     *
+     * @param dpValue dp value
+     * @return px value
+
+     */
+    public static int dp2px(final float dpValue , Context context) {
+        final float scale = context.getResources().getDisplayMetrics().density;
+        return (int) (dpValue * scale + 0.5f);
+    }
+
+    /**
+     * px to dp
+     *
+     * @param pxValue px value
+     * @return dp value
+
+     */
+    public static int px2dp(final float pxValue , Context context) {
+        final float scale =context.getResources().getDisplayMetrics().density;
+        return (int) (pxValue / scale + 0.5f);
+    }
+
+    /**
+     * sp to px
+     *
+     * @param spValue sp value
+     * @return px value
+
+     */
+    public static int sp2px(final float spValue , Context context) {
+        final float fontScale = context.getResources().getDisplayMetrics().scaledDensity;
+        return (int) (spValue * fontScale + 0.5f);
+    }
+
+    /**
+     * px to sp
+     *
+     * @param pxValue px value
+
+     * @return sp value
+
+     */
+    public static int px2sp(final float pxValue , Context context) {
+        final float fontScale = context.getResources().getDisplayMetrics().scaledDensity;
+        return (int) (pxValue / fontScale + 0.5f);
+    }
+
+    /**
+     * Various units of conversion
+     * <p>The method exists in TypedValue</p>
+     *
+     * @param unit    unit
+     * @param value   value
+     * @param metrics DisplayMetrics
+     * @return Conversion results
+
+     */
+    public static float applyDimension(final int unit, final float value, final DisplayMetrics metrics) {
+        switch (unit) {
+            case TypedValue.COMPLEX_UNIT_PX:
+                return value;
+            case TypedValue.COMPLEX_UNIT_DIP:
+                return value * metrics.density;
+            case TypedValue.COMPLEX_UNIT_SP:
+                return value * metrics.scaledDensity;
+            case TypedValue.COMPLEX_UNIT_PT:
+                return value * metrics.xdpi * (1.0f / 72);
+            case TypedValue.COMPLEX_UNIT_IN:
+                return value * metrics.xdpi;
+            case TypedValue.COMPLEX_UNIT_MM:
+                return value * metrics.xdpi * (1.0f / 25.4f);
+        }
+        return 0;
+    }
+
+    /**
+     * Get the dimensions of the view in onCreate
+     * <p>Need to callback onGetSizeListener interface, in the onGetSize view width and width</p>
+     * <p>Examples of usage are shown below</p>
+     * <pre>
+     * SizeUtils.forceGetViewSize(view, new SizeUtils.onGetSizeListener() {
+     *     Override
+     *     public void onGetSize(final View view) {
+     *         view.getWidth();
+     *     }
+     * });
+     * </pre>
+     *
+     * @param view     view
+     * @param listener Listener
+     */
+    public static void forceGetViewSize(final View view, final onGetSizeListener listener) {
+        view.post(new Runnable() {
+            @Override
+            public void run() {
+                if (listener != null) {
+                    listener.onGetSize(view);
+                }
+            }
+        });
+    }
+
+    /**
+     * Get the monitor for View size
+     */
+    public interface onGetSizeListener {
+        void onGetSize(View view);
+    }
+
+    /**
+     * Measure the view size
+
+     *
+     * @param view view
+     * @return arr[0]: View width, arr [1]: view height
+
+     */
+    public static int[] measureView(final View view) {
+        ViewGroup.LayoutParams lp = view.getLayoutParams();
+        if (lp == null) {
+            lp = new ViewGroup.LayoutParams(
+                    ViewGroup.LayoutParams.MATCH_PARENT,
+                    ViewGroup.LayoutParams.WRAP_CONTENT
+            );
+        }
+        int widthSpec = ViewGroup.getChildMeasureSpec(0, 0, lp.width);
+        int lpHeight = lp.height;
+        int heightSpec;
+        if (lpHeight > 0) {
+            heightSpec = View.MeasureSpec.makeMeasureSpec(lpHeight, View.MeasureSpec.EXACTLY);
+        } else {
+            heightSpec = View.MeasureSpec.makeMeasureSpec(0, View.MeasureSpec.UNSPECIFIED);
+        }
+        view.measure(widthSpec, heightSpec);
+        return new int[]{view.getMeasuredWidth(), view.getMeasuredHeight()};
+    }
+
+    /**
+     * Get the width of the measurement view
+     *
+     * @param view view
+     * @return view width
+     */
+    public static int getMeasuredWidth(final View view) {
+        return measureView(view)[0];
+    }
+
+    /**
+     * Get the measurement view height
+     *
+     * @param view view
+     * @return view height
+     */
+
+
+    public static int getMeasuredHeight(final View view) {
+        return measureView(view)[1];
+    }
+}
