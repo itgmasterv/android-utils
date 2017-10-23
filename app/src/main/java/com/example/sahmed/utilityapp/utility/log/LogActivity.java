9ae25339aa6c4126a995ec4498ceb3f9b07b1d11
syasein <syasien@egypt.itgsolutions.com>
2017-10-23 15:53:31 +0200

    log

---
+++ app/src/main/java/com/example/sahmed/utilityapp/utility/log/LogActivity.java
@@ -0,0 +1,15 @@
+package com.example.sahmed.utilityapp.utility.log;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.example.sahmed.utilityapp.R;
+
+public class LogActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_log);
+    }
+}
